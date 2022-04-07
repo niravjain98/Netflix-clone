04f60d55fad38a8fd2160fdca85c7e310c569962
niravjain98 <niravjain98@gmail.com>
2022-04-07 00:11:03 -0400

    04/06 final edit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import React from 'react';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Banner />
       <Row 
       title = "NETFLIX ORIGINALS" 
       fetchUrl = {requests.fetchNetflixOriginals}
