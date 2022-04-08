69ee93dca3c05a933058b2e10acaf9aca1898ebb
niravjain98 <niravjain98@gmail.com>
2022-04-08 02:52:29 -0400

    Final Edit

--- src/nav.js
+++ src/nav.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import './nav.css'
+import React, {useState,useEffect} from 'react';
+import './nav.css';
 
-function nav() {
-  return (
-    <div className='nav'>
+function Nav() {
+    const [show,handleShow] = useState(false);
+    useEffect(() =>{
+        window.addEventListener("scroll",() =>{
+            if(window.scrollY >100){
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return () =>{
+            window.removeEventListener("scroll");
+        };
+
+    },[]);
+
+    return (
+    <div className={`nav ${show && "nav_black"}`}>
         <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
@@ -18,4 +31,4 @@ function nav() {
   )
 }
 
-export default nav
+export default Nav
